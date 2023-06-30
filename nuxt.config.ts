895eeb5acb668feb410a50a9a95a11f73ffed459
Daniel Chua <chua.s.daniel@gmail.com>
2023-06-30 16:52:46 -0400

    testing again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static',
+    // target: 'static',
     modules: ['@nuxt/content'],
+    app: {
+        head: {
+            title: 'TJ GHI',
+            meta: [
+                {name: 'description', content: 'Club website for GHI'}
+            ],
+            link: [
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+            ]
+        }
+    },
 })
